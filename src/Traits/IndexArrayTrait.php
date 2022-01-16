e07641e24ee3303675e77add554727b424c33a9a
Daniel Hudson <DanielHudson@users.noreply.github.com>
2022-01-16 14:33:38 +0000

    Fix for when parent ID is not published

--- src/Traits/IndexArrayTrait.php
+++ src/Traits/IndexArrayTrait.php
@@ -22,8 +22,7 @@ trait IndexArrayTrait
         $nested = [];
 
         // Create index map & init children array.
-        foreach($index as $key => $value)
-        {
+        foreach ($index as $key => $value) {
             // Map the database Id to the array Id
             $indexMap[$value['id']] = $key;
 
@@ -34,60 +33,54 @@ trait IndexArrayTrait
             $index[$key][$options['active_key']] = false;
 
             // Initialise params.
-            if( ! is_null($index[$key]['params']))
-            {
+            if (!is_null($index[$key]['params'])) {
                 $index[$key]['params'] = json_decode($index[$key]['params'], true);
             }
 
             // Mark as active
-            if( ! is_null($options['slug']))
-            {
+            if (!is_null($options['slug'])) {
                 $active = ($options['slug'] == $index[$key]['slug']) ? true : false;
                 $index[$key][$options['active_key']] = $active;
 
                 // Catch if we need to perform an active scan.
-                if($active) {
+                if ($active) {
                     $activeScan = false;
                 }
             }
         }
 
         // Active Scan - if no slug is marked as active, we scan the first section of the slug.
-        if( ! is_null($options['slug']))
-        {
+        if (!is_null($options['slug'])) {
             $slug = explode('_', $options['slug'], 2);
-            
-            foreach($index as $key => $value)
-            {
+
+            foreach ($index as $key => $value) {
                 $active = ($slug[0] == $index[$key]['slug']) ? true : false;
                 $index[$key][$options['active_key']] = $active;
             }
         }
 
         // Nest index
-        foreach($index as $key => $value)
-        {
+        foreach ($index as $key => $value) {
             $indicesId = $index[$key][$options['parent_key']];
-            
+
             // Add any root items to the nested array.
-            if(is_null($indicesId))
-            {
+            if (is_null($indicesId)) {
                 $nested[] = &$index[$key];
             }
 
             // Add any child items to their parent.
-            if( ! is_null($indicesId))
-            {
+            if (!is_null($indicesId)) {
                 // Get the the array Id of the parent from the Index map.
-                $indexId = $indexMap[$indicesId];
-                
-                // Add the child to the parent.
-                $index[$indexId][$options['child_key']][] = &$index[$key];
+                if (isset($indexMap[$indicesId])) {
+                    $indexId = $indexMap[$indicesId];
 
-                // If the child is active, also mark the parent as active.
-                if($index[$key][$options['active_key']] === true)
-                {
-                    $index[$indexId][$options['active_key']] = $index[$key][$options['active_key']];
+                    // Add the child to the parent.
+                    $index[$indexId][$options['child_key']][] = &$index[$key];
+
+                    // If the child is active, also mark the parent as active.
+                    if ($index[$key][$options['active_key']] === true) {
+                        $index[$indexId][$options['active_key']] = $index[$key][$options['active_key']];
+                    }
                 }
             }
         }
